ac24e44f07808306d47088d3057bc33a6a9b0254
Saif <saifisami2005@gmail.com>
2024-10-29 00:14:36 -0400

    Connected the invoice router to app.js.

---
+++ routes/invoices.js
@@ -0,0 +1,6 @@
+const db = require("../db");
+const ExpressError = require("../expressError");
+
+const express = require("express"), router = new express.Router();
+
+module.exports = { invoiceRoutes: router };
