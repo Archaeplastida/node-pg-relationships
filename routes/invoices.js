4eb5e0e450c0763c86b9c66bf70cf3a7121906b2
Saif <saifisami2005@gmail.com>
2024-10-30 03:42:29 -0400

    Minor fixes.

--- routes/invoices.js
+++ routes/invoices.js
@@ -15,8 +15,8 @@ router.get("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
     try {
         const results = await db.query(`SELECT id, comp_code, amt, paid, add_date, paid_date FROM invoices WHERE id = $1`, [req.params.id]);
-        if (!!results.rows.length) return res.json({ invoice: results.rows });
-        else throw new ExpressError(`Invoice ${req.params.id} doesn't exist.`)
+        if (!!results.rows.length) return res.json({ invoice: results.rows[0] });
+        else throw new ExpressError(`Invoice ${req.params.id} doesn't exist.`, 404);
     } catch (err) {
         return next(err);
     }
@@ -27,7 +27,7 @@ router.post("/", async (req, res, next) => {
         const data = req.body;
         if (data.comp_code && data.amt) {
             result = await db.query(`INSERT INTO invoices (comp_code, amt) VALUES ($1, $2) RETURNING id, comp_code, amt, paid, add_date, paid_date`, [data.comp_code, data.amt]);
-            return res.json({ invoice: result.rows[0] });
+            return res.status(201).json({ invoice: result.rows[0] });
         } else throw new ExpressError("Missing parameter(s).", 400);
     } catch (err) {
         return next(err);
@@ -41,7 +41,7 @@ router.put("/:id", async (req, res, next) => {
         if (!check.rows.length) throw new ExpressError(`Invoice ${req.params.id} doesn't exist.`, 404);
         if (data.amt) {
             const result = await db.query(`UPDATE invoices SET amt=$1 WHERE id=$2 RETURNING id, comp_code, amt, paid, add_date, paid_date`, [data.amt, req.params.id]);
-            return res.json({ invoice: result.rows[0] });
+            return res.status(201).json({ invoice: result.rows[0] });
         } else throw new ExpressError("Missing parameter.", 400);
     } catch (err) {
         return next(err);
@@ -53,7 +53,7 @@ router.delete("/:id", async (req, res, next) => {
         const results = await db.query(`SELECT id FROM invoices WHERE id=$1`, [req.params.id]);
         if (!!results.rows.length) {
             await db.query(`DELETE FROM invoices WHERE id=$1`, [req.params.id]);
-            return res.json({ status: "deleted" });
+            return res.status(200).json({ status: "deleted" });
         } else throw new ExpressError(`Invoice ${req.params.id} doesn't exist.`, 404);
     } catch (err) {
         return next(err);
