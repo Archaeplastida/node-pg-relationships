c0724f53922ae312548e4d463e15e54f56874ab8
Saif <saifisami2005@gmail.com>
2024-10-31 07:26:21 -0400

    Added extra data to add in, for the table which shall be used in the new route.

--- data.sql
+++ data.sql
@@ -1,7 +1,9 @@
 \c biztime
 
+DROP TABLE IF EXISTS industries_companies;
 DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
 
 CREATE TABLE companies (
     code text PRIMARY KEY,
@@ -19,12 +21,43 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
+CREATE TABLE industries (
+    code text PRIMARY KEY,
+    industry text NOT NULL UNIQUE
+);
+
+CREATE TABLE industries_companies (
+  industry_code TEXT NOT NULL REFERENCES industries ON DELETE CASCADE,
+  company_code TEXT NOT NULL REFERENCES companies ON DELETE CASCADE,
+  PRIMARY KEY(industry_code, company_code)
+);
+
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
-         ('ibm', 'IBM', 'Big blue.');
+         ('ibm', 'IBM', 'Big blue.'),
+         ('walmart', 'Walmart', 'Multinational Retail.'),
+         ('bank-of-america', 'Bank of America', 'Investment bank and financial services.');
 
 INSERT INTO invoices (comp_Code, amt, paid, paid_date)
   VALUES ('apple', 100, false, null),
          ('apple', 200, false, null),
          ('apple', 300, true, '2018-01-01'),
-         ('ibm', 400, false, null);
+         ('ibm', 400, false, null),
+         ('ibm', 200, true, '2019-05-20'),
+         ('walmart', 150, true, '2020-11-20'),
+         ('walmart', 300, false, null),
+         ('walmart', 1000, false, null),
+         ('bank-of-america', 800, false, null);
+
+INSERT INTO industries
+  VALUES ('acct', 'Accounting'),
+         ('rt', 'Retailing'),
+         ('sfe', 'Software Engineering');
+
+INSERT INTO industries_companies
+  VALUES ('sfe', 'apple'),
+         ('rt', 'apple'),
+         ('sfe', 'ibm'),
+         ('rt', 'walmart'),
+         ('acct', 'bank-of-america'),
+         ('sfe', 'bank-of-america');
